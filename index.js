1be4fbc92c9e33c234f963a459d94a6cbe32966e
Erika Harvey <erikaharvey@ufl.edu>
2026-03-31 12:42:38 -0400

    added to js

--- index.js
+++ index.js
@@ -9,10 +9,34 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+function makePoemHTML(poemJSON) {
+  const poem = poemJSON[0];
+  const { title, author, lines } = poem;
+
+  //tags for title & author
+  const titleHTML = makeTag('h2')(title);
+  const authorHTML = makeTag('h3')(makeTag('em')(`by ${author}`));
+
+  // split lines into stanzas, then convert each stanza to a paragraph with <br> tags for line breaks
+  const linesToStanzas = lines => lines.reduce((stanzas, line) => {
+    if (line.trim() === '') {
+      stanzas.push([])
+    } else {
+      stanzas[stanzas.length - 1].push(line)
+    }
+    return stanzas
+  }, [[]])
+
+  const stanzaToHTML = (stanzas) => makeTag('p')(stanzas.join('<br>'))
+
+  //use pipe to build poem
+  const poemBody = pipe(linesToStanzas, (stanzas) => stanzas.map(stanzaToHTML), (paragraphs) => paragraphs.join(''))(poem.lines);
+
+  return titleHTML + authorHTML + poemBody
+}
 
 // attach a click event to #get-poem
-getPoemBtn.onclick = async function() {
+getPoemBtn.onclick = async function () {
   // renders the HTML string returned by makePoemHTML to #poem
   poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
 }
